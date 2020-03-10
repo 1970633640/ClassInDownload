54bebf6aa473015773d2e77286c81b22e479fbae
1970633640 <>
2020-03-10 22:51:17 +0800

    添加项目文件。

---
+++ ClassInDownload/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ClassInDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 441);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(662, 27);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(11, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(528, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "解析链接 Preview!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(425, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "粘贴 Paste";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LessonKey: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "info:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column5,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(662, 219);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(11, 336);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(224, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Only download video>10min";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Download?";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "StartTime";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 200;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Duration";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "FileSize";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "URL";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column4.Width = 1000;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(11, 366);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(440, 26);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "D:\\ClassIn";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(460, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 26);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "选择目录 Download folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(11, 398);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(662, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "下载已选择视频！ Download selected videos!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(13, 473);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 27);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Waiting";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(13, 514);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 21);
+            this.label4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 559);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "ClassIn 下载器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ClassInDownload/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace ClassInDownload
+{
+
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        ClassInObject item;
+        String downloadFolder="D:\\ClassIn";
+        String folder;
+        int downloadTaskCount;
+        int currentTaskNum;
+        int currentVideoNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public class ClassInObject
+        {
+            public Data data { get; set; }
+        }
+
+
+        public class Data
+        {
+            public string lessonId { get; set; }
+            public string lessonName { get; set; }
+            public Lessondata lessonData { get; set; }
+            public string lessonStarttime { get; set; }
+            public string lessonEndtime { get; set; }
+            public string courseName { get; set; }
+            public string schoolName { get; set; }
+            public string teacherName { get; set; }
+        }
+
+        public class Lessondata
+        {
+            public int lessonStatus { get; set; }
+            public Filelist[] fileList { get; set; }
+        }
+
+
+        public class Filelist
+        {
+            public string CreateTime { get; set; }
+            public int Duration { get; set; }
+            public string EndTime { get; set; }
+            public string FileId { get; set; }
+            public string FileName { get; set; }
+            public string Message { get; set; }
+            public Playset[] Playset { get; set; }
+            public string Size { get; set; }
+            public int SourceType { get; set; }
+            public string StartTime { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class Playset
+        {
+            public string Definition { get; set; }
+            public string Url { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            if(s.IndexOf("lessonKey=")==-1)
+            {
+                return;
+            }
+            string lessonkey = s.Substring(s.IndexOf("lessonKey=") + 10);
+            label1.Text = "LessonKey: " + lessonkey;
+
+            //net
+            var values = new Dictionary<string, string>
+            {
+                { "lessonKey", lessonkey },
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("https://www.eeo.cn/saasajax/webcast.ajax.php?action=getLessonLiveInfo", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            //json
+            item = JsonSerializer.Deserialize<ClassInObject>(responseString);
+
+            //ui
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 8, 0, 0, 0);
+            var startTime = dateTime.AddSeconds(double.Parse(item.data.lessonStarttime));
+            var endTime = dateTime.AddSeconds(double.Parse(item.data.lessonEndtime));
+            label2.Text = "School: " + item.data.schoolName + "    Teacher: " + item.data.teacherName + "    LessonName: " + item.data.lessonName
+                + "\n(GMT+8) Start: " + startTime.ToShortDateString() + " " + startTime.ToLongTimeString()
+                + "    End: " + endTime.ToShortDateString() + " " + endTime.ToLongTimeString();
+            folder = item.data.lessonName;
+            refreshText2();
+
+            //file data grid
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < item.data.lessonData.fileList.Length; i++)
+            {
+                var it = item.data.lessonData.fileList[i];
+                dataGridView1.Rows.Add((it.Duration>600||(!checkBox1.Checked)),it.StartTime, it.Duration/3600+"hr"+(it.Duration%3600)/60+"min", long.Parse(it.Size)/1024/1024+"MB", it.Playset[0].Url);
+            }
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                if(dataGridView1.Rows.Count>0)
+                {
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        if (item.data.lessonData.fileList[i].Duration > 600)
+                            dataGridView1.Rows[i].Cells[0].Value = true;
+                        else
+                            dataGridView1.Rows[i].Cells[0].Value = false;
+                    }
+                }
+            }
+            else
+            {
+                for(int i=0;i<dataGridView1.Rows.Count;i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Download Root Folder";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    return;
+                }
+                downloadFolder = dialog.SelectedPath;
+                refreshText2();
+            }
+        }
+        private void refreshText2()
+        {
+            textBox2.Text = downloadFolder+"\\"+folder;
+        }
+
+        private void refreshStatus(string x,string a)
+        {
+            label4.Text = a;
+            label3.Text = (currentTaskNum+1).ToString() + "/" + downloadTaskCount + "    " + x + "%";
+            int x2=0;
+            int.TryParse(x,out x2);
+            progressBar1.Value = x2;
+            if (currentVideoNum >= item.data.lessonData.fileList.Length)
+                label3.Text = "成功！ Complete!";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            downloadTaskCount = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[0].Value)==true)
+                    downloadTaskCount += 1;
+            }
+            currentTaskNum = -1;
+            currentVideoNum = -1;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            checkBox1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            down();
+        }
+        private void success()
+        {
+            if (currentVideoNum >= item.data.lessonData.fileList.Length)
+                label3.Text = "成功！ Complete!";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            checkBox1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+        private void down()
+        {
+            currentTaskNum += 1;
+            currentVideoNum += 1;
+            if (currentVideoNum >= item.data.lessonData.fileList.Length)
+            {
+                success();
+                return;
+            }
+            while (Convert.ToBoolean(dataGridView1.Rows[currentVideoNum].Cells[0].Value) == false)
+                currentVideoNum += 1;
+            if (currentVideoNum >= item.data.lessonData.fileList.Length)
+            {
+                success();
+                return;
+            }
+            refreshStatus("0","");
+            DownloadFileByAria2(item.data.lessonData.fileList[currentVideoNum].Playset[0].Url, textBox2.Text, item.data.lessonData.fileList[currentVideoNum].StartTime.Replace(':','-')+".mp4");
+        }
+
+        //https://www.cnblogs.com/littlehb/p/5782714.html
+        public  bool DownloadFileByAria2(string url, string strFileName1,string strFileName2)
+        {
+            var tool = System.AppDomain.CurrentDomain.BaseDirectory+"data\\aria2c\\aria2c.exe";
+            var command = " -c -x 8  --file-allocation=falloc -d \"" +strFileName1 + "\" -o \"" + strFileName2 + "\" " + url;
+            using (var p = new Process())
+            {
+                RedirectExcuteProcess(p, tool, command, (s, e) => ShowInfo(url, e.Data));
+            }
+            return true;
+        }
+        private  void ShowInfo(string url, string a)
+        {
+            if (a == null) return;
+
+            const string re1 = ".*?"; // Non-greedy match on filler
+            const string re2 = "(\\(.*\\))"; // Round Braces 1
+
+            var r = new Regex(re1 + re2, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            var m = r.Match(a);
+            if (m.Success)
+            {
+                var rbraces1 = m.Groups[1].ToString().Replace("(", "").Replace(")", "").Replace("%", "").Replace("s", "0");
+                //Console.WriteLine("原始" + a);
+                //Console.WriteLine(rbraces1);
+                if (rbraces1 == "OK")
+                {
+                    rbraces1 = "100";
+                    refreshStatus(rbraces1,a);
+                    down();
+                }
+                refreshStatus(rbraces1,a);
+            }
+        }
+
+        /// <summary>
+        /// 功能：重定向执行
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="exe"></param>
+        /// <param name="arg"></param>
+        /// <param name="output"></param>
+        private  void RedirectExcuteProcess(Process p, string exe, string arg, DataReceivedEventHandler output)
+        {
+            p.StartInfo.FileName = exe;
+            p.StartInfo.Arguments = arg;
+
+            p.StartInfo.UseShellExecute = false;    //输出信息重定向
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+
+            p.OutputDataReceived += output;
+            p.ErrorDataReceived += output;
+
+            p.Start();                    //启动线程
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+            p.WaitForExit();            //等待进程结束
+        }
+    }
+}
